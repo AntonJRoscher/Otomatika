f3d6ac3b25e9985395488fc8afb03e32239fa634
AntonJRoscher <antonjohanro@gmail.com>
2024-04-14 20:14:20 +0200

    OOP based implementation

--- scripts/news_scraper.py
+++ scripts/news_scraper.py
@@ -11,42 +11,91 @@ from selenium.webdriver.support.ui import Select
 import time 
 from datetime import datetime
 
-driver = webdriver.Firefox()
-driver.get("https://apnews.com/")
-start = time.process_time()
-# try:
-search_button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//button[@class='SearchOverlay-search-button']"))).click()
-print(time.process_time() - start)
-searchbox = WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.XPATH, "//input[@name='q' and @class='SearchOverlay-search-input']")))
-print(time.process_time() - start)
-searchbox.send_keys("trump")
-searcbox_button_submit= WebDriverWait(driver,2).until(EC.element_to_be_clickable((By.XPATH,"//button[@class='SearchOverlay-search-submit' and @type='submit']"))).click()
-filter = WebDriverWait(driver,2).until(EC.element_to_be_clickable((By.XPATH,"//select[@class='Select-input' and @name='s']")))
-select_opions = Select(filter).select_by_visible_text('Newest')
+# Type Imports
+from typing import Literal,List
+
+from dataclasses import dataclass
+
+@dataclass
+class NewsStory:
+    title:str
+    content:str
+    date_created:datetime
+
+class NewsScraper():
+    def __init__(self, web_driver=Literal['Firefox','Chrome','Safari','Edge']) -> None:
+        self.webdriver = web_driver
+
+    def connect_driver(self, web_driver:str, url:str="https://apnews.com/") -> webdriver:
+        if web_driver is not None:
+            if web_driver == 'Firefox':
+                driver = webdriver.Firefox()
+            elif web_driver == 'Chrome':
+                driver = webdriver.Chrome()
+            elif web_driver == 'Safari':
+                driver = webdriver.Safari()
+            elif web_driver == 'Edge':
+                driver = webdriver.Edge()
+        else:
+            driver = self.webdriver
+
+        driver.get(url)
+        return driver
+
+    def find_story_elements(self, driver:webdriver):
+        stories = {}
+
+        try:
+            search_button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//button[@class='SearchOverlay-search-button']")))
+            search_button.click()
 
 
-# story_items = driver.find_elements(By.XPATH,"//main[@class='SearchResultsModule-main']/div[@class='SearchResultsModule-results']/bsp-list-loadmore[@class='PageListStandardD']/div[@class='PageList-items']/div[@class='PageList-items-item']")
-story_items = WebDriverWait(driver,20).until(EC.visibility_of_all_elements_located((By.XPATH, "//main[@class='SearchResultsModule-main']/div[@class='SearchResultsModule-results']/bsp-list-loadmore[@class='PageListStandardD']/div[@class='PageList-items']/div[@class='PageList-items-item']")))
+            searchbox = WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.XPATH, "//input[@name='q' and @class='SearchOverlay-search-input']")))
+            searchbox.send_keys("trump")
+            
+            searchbox_button_submit= WebDriverWait(driver,2).until(EC.element_to_be_clickable((By.XPATH,"//button[@class='SearchOverlay-search-submit' and @type='submit']")))
+            searchbox_button_submit.click()
 
-for story in story_items:
-    story_content = story.find_element(By.XPATH,"./div[@class='PagePromo']/div[@class='PagePromo-content']") #.get_attribute('innerHTML')
-    story_title = story_content.find_element(By.XPATH,"./bsp-custom-headline[@custom-headline='div']/div[@class='PagePromo-title']/a[@class='Link ']/span[@class='PagePromoContentIcons-text']").text
-    story_description = story_content.find_element(By.XPATH,"./div[@class='PagePromo-description']/a[@class='Link ']/span[@class='PagePromoContentIcons-text']").text
-    story_updated_date = story_content.find_element(By.XPATH,"./div[@class='PagePromo-byline']/div[@class='PagePromo-date']/bsp-timestamp/span").text
-    story_timestamp = story_content.find_element(By.XPATH,"./div[@class='PagePromo-byline']/div[@class='PagePromo-date']/bsp-timestamp").get_attribute('data-timestamp')
-    timestamp = datetime.utcfromtimestamp(float(story_timestamp)/1000)
-    # story_title = story_content.find_element(By.XPATH,"//div[@class='PagePromo-title']/a/span[@class='PagePromoContentIcons-text']").text
+            filter = WebDriverWait(driver,2).until(EC.element_to_be_clickable((By.XPATH,"//select[@class='Select-input' and @name='s']")))
+            Select(filter).select_by_visible_text('Newest')
 
-    print(f""" 
-            {story_title} \n
-            {story_description} \n
-            # {story_updated_date} \n
-            {timestamp} \n
-    
-    """) # USE DATA CLASS TO STORE INFO 
 
-# finally:
+            # story_items = driver.find_elements(By.XPATH,"//main[@class='SearchResultsModule-main']/div[@class='SearchResultsModule-results']/bsp-list-loadmore[@class='PageListStandardD']/div[@class='PageList-items']/div[@class='PageList-items-item']")
+            story_items = WebDriverWait(driver,20).until(EC.visibility_of_all_elements_located((By.XPATH, "//main[@class='SearchResultsModule-main']/div[@class='SearchResultsModule-results']/bsp-list-loadmore[@class='PageListStandardD']/div[@class='PageList-items']/div[@class='PageList-items-item']")))
 
-driver.close()
+            for story in story_items:
+                story_content = story.find_element(By.XPATH,"./div[@class='PagePromo']/div[@class='PagePromo-content']") #.get_attribute('innerHTML')
+                story_title = story_content.find_element(By.XPATH,"./bsp-custom-headline[@custom-headline='div']/div[@class='PagePromo-title']/a[@class='Link ']/span[@class='PagePromoContentIcons-text']").text
+                story_description = story_content.find_element(By.XPATH,"./div[@class='PagePromo-description']/a[@class='Link ']/span[@class='PagePromoContentIcons-text']").text
+                # story_updated_date = story_content.find_element(By.XPATH,"./div[@class='PagePromo-byline']/div[@class='PagePromo-date']/bsp-timestamp/span").text
+                story_timestamp = story_content.find_element(By.XPATH,"./div[@class='PagePromo-byline']/div[@class='PagePromo-date']/bsp-timestamp").get_attribute('data-timestamp')
+                timestamp = datetime.utcfromtimestamp(float(story_timestamp)/1000)
+                # story_title = story_content.find_element(By.XPATH,"//div[@class='PagePromo-title']/a/span[@class='PagePromoContentIcons-text']").text
+
+                # print(f""" 
+                #         {story_title} \n
+                #         {story_description} \n
+                #         # {story_updated_date} \n
+                #         {timestamp} \n
+                
+                # """) # USE DATA CLASS TO STORE INFO 
+
+                stories[story_title] = NewsStory(title=story_title,content=story_description,date_created=timestamp)
+
+        finally:
+            driver.close()
+
+        return stories
 
 # SearchResultsModule-formInput
+
+def main():
+    news_scraper = NewsScraper(web_driver='Firefox')
+    driver = news_scraper.connect_driver(web_driver=news_scraper.webdriver)
+    stories = news_scraper.find_story_elements(driver=driver)
+
+    for story in stories:
+        print(story)
+
+if __name__=="__main__":
+    main()
