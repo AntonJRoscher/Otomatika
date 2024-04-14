bf76366564ac31e9f92ebbd9b3a8eb90030956e6
AntonJRoscher <antonjohanro@gmail.com>
2024-04-14 22:38:16 +0200

    Web Driver Assignment Fix

--- scripts/news_scraper.py
+++ scripts/news_scraper.py
@@ -13,8 +13,9 @@ from datetime import datetime
 
 # Type Imports
 from typing import Literal
-
 from dataclasses import dataclass
+
+# Request Import For 
 
 @dataclass
 class NewsStory:
@@ -24,9 +25,16 @@ class NewsStory:
 
 class NewsScraper():
     def __init__(self, web_driver='Firefox') -> None:
-        self.webdriver = web_driver
+        self.web_driver_type = web_driver
+        self.webdriver = self.type_driver(web_driver=web_driver)
 
-    def connect_driver(self, web_driver:str = Literal['Firefox','Chrome','Safari','Edge'], url:str="https://apnews.com/") -> webdriver:
+    # def child_driver(self, url:str) -> webdriver:
+    #     child_driver = self.webdriver
+    #     child_driver.get(url)
+        
+    #     return child_driver
+
+    def type_driver(self, web_driver:str = Literal['Firefox','Chrome','Safari','Edge']) -> webdriver:
         if web_driver is not None:
             if web_driver == 'Firefox':
                 driver = webdriver.Firefox()
@@ -37,43 +45,106 @@ class NewsScraper():
             elif web_driver == 'Edge':
                 driver = webdriver.Edge()
         else:
-            driver = self.webdriver
+            driver = webdriver.Firefox()
 
+        return driver
+
+    def connect_driver(self, url:str="https://apnews.com/") -> webdriver:
+        driver = self.webdriver
         driver.get(url)
         return driver
 
-    def find_story_elements(self, driver:webdriver):
+    def find_story_elements(self, driver: webdriver):
         stories = {}
 
         try:
-            search_button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//button[@class='SearchOverlay-search-button']")))
+            search_button = WebDriverWait(driver, 2).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//button[@class='SearchOverlay-search-button']")
+                )
+            )
             search_button.click()
 
-
-            searchbox = WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.XPATH, "//input[@name='q' and @class='SearchOverlay-search-input']")))
+            searchbox = WebDriverWait(driver, 4).until(
+                EC.element_to_be_clickable(
+                    (
+                        By.XPATH,
+                        "//input[@name='q' and @class='SearchOverlay-search-input']",
+                    )
+                )
+            )
             searchbox.send_keys("trump")
-            
-            searchbox_button_submit= WebDriverWait(driver,2).until(EC.element_to_be_clickable((By.XPATH,"//button[@class='SearchOverlay-search-submit' and @type='submit']")))
+
+            searchbox_button_submit = WebDriverWait(driver, 2).until(
+                EC.element_to_be_clickable(
+                    (
+                        By.XPATH,
+                        "//button[@class='SearchOverlay-search-submit' and @type='submit']",
+                    )
+                )
+            )
             searchbox_button_submit.click()
 
-            filter = WebDriverWait(driver,2).until(EC.element_to_be_clickable((By.XPATH,"//select[@class='Select-input' and @name='s']")))
-            Select(filter).select_by_visible_text('Newest')
-
+            filter = WebDriverWait(driver, 2).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//select[@class='Select-input' and @name='s']")
+                )
+            )
+            Select(filter).select_by_visible_text("Newest")
 
             # story_items = driver.find_elements(By.XPATH,"//main[@class='SearchResultsModule-main']/div[@class='SearchResultsModule-results']/bsp-list-loadmore[@class='PageListStandardD']/div[@class='PageList-items']/div[@class='PageList-items-item']")
-            story_items = WebDriverWait(driver,20).until(EC.visibility_of_all_elements_located((By.XPATH, "//main[@class='SearchResultsModule-main']/div[@class='SearchResultsModule-results']/bsp-list-loadmore[@class='PageListStandardD']/div[@class='PageList-items']/div[@class='PageList-items-item']")))
+            story_items = WebDriverWait(driver, 20).until(
+                EC.visibility_of_all_elements_located(
+                    (
+                        By.XPATH,
+                        "//main[@class='SearchResultsModule-main']/div[@class='SearchResultsModule-results']/bsp-list-loadmore[@class='PageListStandardD']/div[@class='PageList-items']/div[@class='PageList-items-item']",
+                    )
+                )
+            )
 
-            for index,story in enumerate(story_items):
+            for index, story in enumerate(story_items):
                 try:
-                    story_content = story.find_element(By.XPATH,"./div[@class='PagePromo']/div[@class='PagePromo-content']") #.get_attribute('innerHTML')
-                    story_title = story_content.find_element(By.XPATH,"./bsp-custom-headline[@custom-headline='div']/div[@class='PagePromo-title']/a[@class='Link ']/span[@class='PagePromoContentIcons-text']").text
-                    story_description = story_content.find_element(By.XPATH,"./div[@class='PagePromo-description']/a[@class='Link ']/span[@class='PagePromoContentIcons-text']").text
+                    story_content = story.find_element(
+                        By.XPATH,
+                        "./div[@class='PagePromo']/div[@class='PagePromo-content']",
+                    )
 
-                    story_timestamp = story_content.find_element(By.XPATH,"./div[@class='PagePromo-byline']/div[@class='PagePromo-date']/bsp-timestamp").get_attribute('data-timestamp')
-                    timestamp = datetime.utcfromtimestamp(float(story_timestamp)/1000)
+                    story_title = story_content.find_element(
+                        By.XPATH,
+                        "./bsp-custom-headline[@custom-headline='div']/div[@class='PagePromo-title']/a[@class='Link ']/span[@class='PagePromoContentIcons-text']",
+                    ).text
 
+                    story_description = story_content.find_element(
+                        By.XPATH,
+                        "./div[@class='PagePromo-description']/a[@class='Link ']/span[@class='PagePromoContentIcons-text']",
+                    ).text
 
-                    stories[index] = NewsStory(title=story_title,content=story_description,date_created=timestamp)
+                    story_timestamp = story_content.find_element(
+                        By.XPATH,
+                        "./div[@class='PagePromo-byline']/div[@class='PagePromo-date']/bsp-timestamp",
+                    ).get_attribute("data-timestamp")
+                    timestamp = datetime.utcfromtimestamp(float(story_timestamp) / 1000)
+
+                    # story_img_url = story.find_element(
+                    #     By.XPATH,
+                    #     "./div[@class='PagePromo']/div[@class='PagePromo-media']/a/picture/img[@class='Image']",
+                    # ).get_attribute("src")
+
+                    # download_img_driver = self.connect_driver(child_driver=True, url=story_img_url)
+                    # img_to_write = download_img_driver.find_element(
+                    #             By.TAG_NAME, "body"
+                    #         ).screenshot_as_png
+                    # with open("image_name.jpg", "wb") as file:
+                    #     file.write(
+                    #         img_to_write
+                    #     )
+                    # download_img_driver.close()
+
+                    stories[index] = NewsStory(
+                        title=story_title,
+                        content=story_description,
+                        date_created=timestamp,
+                    )
                 except StaleElementReferenceException:
                     return None
 
@@ -82,15 +153,16 @@ class NewsScraper():
 
         return stories
 
+
 def main():
     news_scraper = NewsScraper(web_driver='Firefox')
-    driver = news_scraper.connect_driver(web_driver=news_scraper.webdriver)
+    driver = news_scraper.connect_driver()
     stories = news_scraper.find_story_elements(driver=driver)
 
     if stories is None:
         print("No stories found - re-running web scraping activity")
-        driver.close() # Close current open connection
-        driver = news_scraper.connect_driver(web_driver=news_scraper.webdriver)
+        # driver.close() # Close current open connection
+        driver = news_scraper.connect_driver()
         stories = news_scraper.find_story_elements(driver=driver)
     else:
          for story in stories.values():
